e2cd0b974051b9a26c1a2bb959aeaf8f6e5ec2b2
Sp7dey520 <103517937+kwchun0520@users.noreply.github.com>
2024-08-07 17:31:17 +0800

    Update storage.py (#2)
    updated the storage to allow upload df as csv

--- minihelper/gcp/storage.py
+++ minihelper/gcp/storage.py
@@ -2,7 +2,7 @@ from io import BytesIO, StringIO
 from loguru import logger
 from google.cloud import storage
 import google.auth
-from typing import Union, BinaryIO
+from typing import Union
 
 SCOPES = [
  "https://storage.googleapis.com/auth/storage"   
@@ -18,14 +18,14 @@ def _client() -> storage.Client:
     creds, proj = google.auth.default(SCOPES)
     return storage.Client(project=proj, credentials=creds)
 
-def upload_from_string(data: Union[StringIO, BytesIO], bucket: str, path: str, content_type: str = 'text/plain') -> None:
+def upload_from_string(data: Union[StringIO, BytesIO], bucket: str, path: str, content_type: str = "text/plain") -> None:
     """Upload file to storage bucket from string/bytes
 
     Args:
         data (Union[StringIO, BytesIO]): data to be uploaded
         bucket (str): destination bucket
         path (str): destination path
-        content_type (str, optional): content type of data. Defaults to 'text/plain'.
+        content_type (str, optional): content type of data. Defaults to "text/plain".
     """
 
     logger.debug(f"Loading bucket: {bucket}")
@@ -33,23 +33,6 @@ def upload_from_string(data: Union[StringIO, BytesIO], bucket: str, path: str, c
     bucket = _client().bucket(bucket)
     bucket.blob(path).upload_from_string(data=data, content_type=content_type)
     
-    logger.info(f"Upload file success: gs://{bucket}/{path}")
-
-
-def upload_from_file(file: BinaryIO, bucket: str, path: str) -> None:
-    """Upload file to storage bucket from file
-
-    Args:
-        file (BinaryIO): file object in binary format
-        bucket (str): destination bucket
-        path (str): destination_path
-    """
-
-    logger.debug(f"Loading bucket: {bucket}")
-
-    bucket = _client().bucket(bucket)
-    bucket.blob(path).upload_from_file(file)
-
     logger.info(f"Upload file success: gs://{bucket}/{path}")
 
 
@@ -68,3 +51,20 @@ def upload_from_filename(local_path: str, bucket: str, destination_path: str) ->
     bucket.blob(destination_path).uplod_from_filename(local_path)
 
     logger.info(f"Upload file success: gs://{bucket}/{destination_path}")
+
+
+def upload_df_as_csv(df: pd.DataFrame, bucket: str, path:str) -> None:
+    """Upload csv file from a pd.DataFrame
+
+    Args:
+         df (pd.DataFrame): dataframe
+         bucket (str): destination bucket
+         path (str): destination file path
+    """
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer,index = False)
+    csv_buffer.seek(0)
+
+    upload_from_string(data=csv_buffer, bucket=bucket, path=path content_type="text/csv")
+
+   
